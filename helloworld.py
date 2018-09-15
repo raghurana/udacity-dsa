23d6d43455c5cb862ae495a1ad31c3e84c198c8d
Raghu Rana <raghurana@gmail.com>
2018-09-15 18:46:19 +1000

    len

--- helloworld.py
+++ helloworld.py
@@ -6,6 +6,9 @@ simpleList = [1, 2, 3]
 for item in simpleList:
     print(item)
 
+print('List Length')
+print (len(simpleList))
+
 class Calculator:
     def __init__(self, a, b):
             self.num1 = a
