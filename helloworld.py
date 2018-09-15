40005c5da976b39cd8db6a3210ec158fe4c8cdea
Raghu Rana <raghurana@gmail.com>
2018-09-15 11:31:55 +1000

    Basics 1

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,23 @@
 
-
 msg = "hello world"
-
 print(msg.upper())
+
+simpleList = [1, 2, 3]
+for item in simpleList:
+    print(item)
+
+class Calculator:
+    def __init__(self, a, b):
+            self.num1 = a
+            self.num2 = b
+    
+    def add(self):
+        return self.num1 + self.num2
+
+x = Calculator(50, 100)
+print(x.add())
+
+
+
+
+
